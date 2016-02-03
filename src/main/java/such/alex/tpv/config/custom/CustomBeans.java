4f451a2ad2fa320cc5e18c3232f05c601a5cd6b4
Alejandro Such <alejandro.such@gmail.com>
2016-02-03 09:39:15 +0100

    eventbus is now managed by spring-reactor

--- src/main/java/such/alex/tpv/config/custom/CustomBeans.java
+++ src/main/java/such/alex/tpv/config/custom/CustomBeans.java
@@ -4,27 +4,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import reactor.Environment;
 import reactor.bus.EventBus;
+import reactor.spring.context.config.EnableReactor;
 
 /**
  * Created by alejandro on 02/02/2016.
  */
 @Configuration
+@EnableReactor
+@EnableAsync
 public class CustomBeans {
     private final Logger log = LoggerFactory.getLogger(CustomBeans.class);
 
-    @Bean
-    Environment env() {
-        return Environment
-            .initializeIfEmpty()
-            .assignErrorJournal();
+    @Bean(name = "eventBus")
+    EventBus getEventBus(Environment env) {
+        return EventBus.create(env, Environment.THREAD_POOL);
     }
-
-    @Bean
-    EventBus createEventBus(Environment env) {
-        return EventBus
-            .create(env, Environment.THREAD_POOL);
-    }
-
 }
