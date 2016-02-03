cc75872de6bc2a759af28950276e7eebb0a13044
Alejandro Such <alejandro.such@gmail.com>
2016-02-03 09:40:53 +0100

    improved method

--- src/main/java/such/alex/tpv/config/custom/repository/HistoricRepositoryImpl.java
+++ src/main/java/such/alex/tpv/config/custom/repository/HistoricRepositoryImpl.java
@@ -17,29 +17,32 @@ import java.io.Serializable;
 @NoRepositoryBean
 public class HistoricRepositoryImpl<T extends HistoricEntity, ID extends Serializable> extends SimpleJpaRepository<T, ID> implements HistoricRepository<T, ID> {
     EntityManager entityManager;
+    JpaEntityInformation entityInformation;
 
     public HistoricRepositoryImpl(JpaEntityInformation<T, ?> entityInformation, EntityManager entityManager) {
         super(entityInformation, entityManager);
         this.entityManager = entityManager;
+        this.entityInformation = entityInformation;
     }
 
     public HistoricRepositoryImpl(Class<T> domainClass, EntityManager em) {
         super(domainClass, em);
         this.entityManager = em;
+        this.entityInformation = JpaEntityInformationSupport.getEntityInformation(domainClass, em);
     }
 
 
     @Override
     public <S extends T> S saveWithHistoric(S historic) {
-        S clone = (S)historic.clone();
-
-        if(entityManager.contains(historic)) {
-            historic.setActive(false);
-            super.save(historic);
+        if (this.entityInformation.isNew(historic)) {
+            return this.save(historic);
+        } else {
+            final S clone = (S) historic.clone();
+            final HistoricEntity reference = this.entityManager.getReference(this.getDomainClass(), entityInformation.getId(historic));
+            reference.setActive(false);
+            super.save((S)reference);
 
             return this.save(clone);
-        } else {
-            return this.save((S)historic);
         }
     }
 }
