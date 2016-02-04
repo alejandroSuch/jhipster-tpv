b0cb57f674692a4097389a94787e26ab7c06b586
Alejandro Such <alejandro.such@gmail.com>
2016-02-04 18:18:27 +0100

    Added new fields to HistoricEntity HistoricEntity is now a @MappedSuperclass

--- src/main/java/such/alex/tpv/domain/HistoricEntity.java
+++ src/main/java/such/alex/tpv/domain/HistoricEntity.java
@@ -1,12 +1,23 @@
 package such.alex.tpv.domain;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
+import java.time.LocalDate;
 
 /**
  * Created by alejandro on 31/01/2016.
  */
+@MappedSuperclass
 public abstract class HistoricEntity implements Serializable, Cloneable {
-    private Boolean active;
+    @Column(name = "active", nullable = false)
+    private Boolean active = Boolean.TRUE;
+
+    @Column(name = "active_from", nullable = true)
+    private LocalDate activeFrom;
+
+    @Column(name = "active_to", nullable = true)
+    private LocalDate activeTo;
 
     public Boolean getActive() {
         return active;
@@ -17,4 +28,20 @@ public abstract class HistoricEntity implements Serializable, Cloneable {
     }
 
     public abstract HistoricEntity clone();
+
+    public LocalDate getActiveFrom() {
+        return activeFrom;
+    }
+
+    public void setActiveFrom(LocalDate activeFrom) {
+        this.activeFrom = activeFrom;
+    }
+
+    public LocalDate getActiveTo() {
+        return activeTo;
+    }
+
+    public void setActiveTo(LocalDate activeTo) {
+        this.activeTo = activeTo;
+    }
 }
